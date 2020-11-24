60f1eed4835296cd1230ea159cb8617881f05d32
Your NameMaha <aggurug10@gmail.com>
2020-11-24 22:52:33 +0800

    db done

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
 module.exports = {
     mongodbUrl : 'mongodb://localhost:27017/cmsdemo'
+    // mongodbUrl : 'mongo "mongodb+srv://cluster0.42cmr.mongodb.net/<dbname>"'
+    // mongo "mongodb+srv://cluster0.42cmr.mongodb.net/<dbname>"
 }
